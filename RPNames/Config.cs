448f6a2e6bce059e024d2dad1f21a1f8831274b7
1EnesBaturKaza <156574288+1EnesBaturKaza@users.noreply.github.com>
2024-02-03 15:58:36 +0300

    Add files via upload

---
+++ RPNames/Config.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using PlayerRoles;
+using Scp914;
+
+namespace RPNames
+{
+    public class Config : IConfig
+    {
+        [Description("Is the plugin enabled?")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Is the Debug on?")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Should people lose nick when they turn into a tutorial?")]
+        public bool TutorialNick { get; set; } = true;
+
+        [Description("Should Class-D be D-#### (true) or should it follow the other names with Title + Name (false)?")]
+        public bool DboisSetting { get; set; } = true;
+
+        [Description("Should SCPs have their SCP as a name (true) or should they be hidden as a D-Class (false)?")]
+        public bool SCPSetting { get; set; } = false;
+
+        [Description("Should Zombies NOT have names? (THIS OVERRIDES SCPSETTINGS)")]
+        public bool ZombieNames { get; set; } = false;
+        
+        [Description("Should nicknames reset on death?")]
+        public bool DeathReset { get; set; } = true;
+
+        [Description("Should a player be told their nickname?")]
+        public bool ShowNick { get; set; } = true;
+
+        [Description("Should a player have there real name after their nickname?")]
+        public bool ShowRealName { get; set; } = false;
+
+        [Description("A dictionary with classes and their title")]
+        public Dictionary<RoleTypeId, string> ClassTitles { get; set; } = new Dictionary<RoleTypeId, string>
+        {
+            {
+                RoleTypeId.ClassD, "D-"
+            },
+            {
+                RoleTypeId.Scientist, "Dr."
+            },
+            {
+                RoleTypeId.FacilityGuard, "Security Officer"
+            },
+            {
+                RoleTypeId.NtfCaptain, "Commander"
+            },
+            {
+                RoleTypeId.NtfPrivate, "Cadet"
+            },
+            {
+                RoleTypeId.NtfSergeant, "Lieutenant"
+            },
+            {
+                RoleTypeId.NtfSpecialist, "Field Agent"
+            },
+            {
+                RoleTypeId.ChaosConscript, "Agent of Chaos"
+            },
+            {
+                RoleTypeId.ChaosMarauder, "Agent of Chaos"
+            },
+            {
+                RoleTypeId.ChaosRepressor, "Agent of Chaos"
+            },
+            {
+                RoleTypeId.ChaosRifleman, "Agent of Chaos"
+            },
+            {
+                RoleTypeId.Scp049, "SCP-049"
+            },
+            {
+                RoleTypeId.Scp0492, "SCP-049-2"
+            },
+            {
+                RoleTypeId.Scp079, "SCP-079"
+            },
+            {
+                RoleTypeId.Scp096, "SCP-096"
+            },
+            {
+                RoleTypeId.Scp106, "SCP-106"
+            },
+            {
+                RoleTypeId.Scp173, "SCP-173"
+            },
+            {
+                RoleTypeId.Scp939, "SCP-939"
+            }
+        };
+
+        [Description("The list of names to be given. (Leave empty for default, if you want to make your own list, look at readme) ")]
+        public List<string> HumanNames { get; set; } = new List<string> {"Woodson", "Winthrop", "Wall", "Wight", "Webb", "Woodard", "Wixx", "Wong", "Whesker", "Wolfenstein", "Rooney", "Russo", "Rude", "Roland", "Reader", "Renshaw", "Rossi", "Riddle", "Ripa", "Richter", "Rosenberg", "Romo", "Ramirez", "Reagan", "Rainwater", "Romirez", "Riker","Abbington", "Axworthy", "Arora", "Tray", "R.Tey", "A.Clef", "Amathor", "Jesus", "Joker", "Lemon", "Boucher", "Enes"};
+
+    }
+}
---
+++ RPNames/EventHandlers.cs
@@ -0,0 +1,123 @@
+﻿using MEC;
+
+namespace RPNames.EventHandlers
+{
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs;
+    using PlayerRoles;
+    using System;
+    using Hints;
+    using System.Collections.Generic;
+    using Exiled.Events.EventArgs.Player;
+
+    public class EventHandlers
+    {
+        private readonly Plugin plugin;
+        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+        Random random = new Random();
+
+        public void OnPlayerChangeRole(ChangingRoleEventArgs ev)
+        {
+            int randnumber = random.Next(1000, 9999);
+            
+            int RandomNameNumber = random.Next(1, plugin.NickNames.Count - 1);
+            
+            if (ev.NewRole == RoleTypeId.Tutorial)
+            {
+                if (plugin.Config.TutorialNick)
+                {
+                    ev.Player.DisplayNickname = null;
+                    return;
+                }
+            }
+            
+            if (ev.NewRole == RoleTypeId.Scp0492 && plugin.Config.ZombieNames)
+            {
+                ev.Player.DisplayNickname = null;
+                if (plugin.Config.ShowNick)
+                {
+                    ev.Player.ShowHint(ev.Player.DisplayNickname);
+                }
+                return;
+            }
+            
+            if (ev.NewRole.GetTeam() != Team.SCPs && plugin.Config.ClassTitles.ContainsKey(ev.NewRole))
+            {
+                if (plugin.Config.ShowRealName && ev.NewRole != RoleTypeId.ClassD && plugin.Config.ShowRealName)
+                { 
+                    ev.Player.DisplayNickname = $"{plugin.Config.ClassTitles[ev.NewRole]} {plugin.NickNames[RandomNameNumber]} ({ev.Player.Nickname})";
+                    if (plugin.Config.ShowNick)
+                    {
+                        ev.Player.ShowHint(ev.Player.DisplayNickname);
+                    }
+                    return;
+                }
+                ev.Player.DisplayNickname = $"{plugin.Config.ClassTitles[ev.NewRole]} {plugin.NickNames[RandomNameNumber]}";
+                   
+                if (plugin.Config.ShowNick)
+                {
+                    ev.Player.ShowHint(ev.Player.DisplayNickname);
+                }
+                
+                
+                if (ev.NewRole == RoleTypeId.ClassD && plugin.Config.DboisSetting)
+                {
+                        ev.Player.DisplayNickname = $"{"D-"+randnumber}";
+                        if (plugin.Config.ShowRealName)
+                        {
+                            ev.Player.DisplayNickname = $"{"D-"+randnumber} ({ev.Player.Nickname})";
+                        }
+                        if (plugin.Config.ShowNick)
+                        {
+                            ev.Player.ShowHint(ev.Player.DisplayNickname);
+                        }
+                }
+            }
+            
+            if (ev.NewRole.GetTeam() == Team.SCPs && plugin.Config.ClassTitles.ContainsKey(ev.NewRole))
+            {
+                if (plugin.Config.SCPSetting.Equals(false))
+                {
+                        if (plugin.Config.ShowRealName)
+                        {
+                            ev.Player.DisplayNickname = $"D-{randnumber} ({ev.Player.Nickname})";
+                               
+                            if (plugin.Config.ShowNick)
+                            {
+                                ev.Player.ShowHint(ev.Player.DisplayNickname);
+                            }
+                            return;
+                        }
+                        ev.Player.DisplayNickname = $"D-{randnumber}";
+                           
+                        if (plugin.Config.ShowNick)
+                        {
+                            ev.Player.ShowHint(ev.Player.DisplayNickname);
+                        }
+                        return;
+                }
+                
+                if (plugin.Config.ShowRealName)
+                {
+                    ev.Player.DisplayNickname = $"{plugin.Config.ClassTitles[ev.NewRole]} ({ev.Player.Nickname})";
+                    if (plugin.Config.ShowNick)
+                    {
+                        ev.Player.ShowHint(ev.Player.DisplayNickname);
+                    }
+                    return;
+                }
+                ev.Player.DisplayNickname = $"{plugin.Config.ClassTitles[ev.NewRole]}";
+                if (plugin.Config.ShowNick)
+                {
+                    ev.Player.ShowHint(ev.Player.DisplayNickname);
+                }
+            }
+        }
+        
+        public void OnPlayerDeath(DiedEventArgs ev)
+        {
+            if (plugin.Config.DeathReset && ev.Player != null) ev.Player.DisplayNickname = null;
+        }
+    }
+}
+
